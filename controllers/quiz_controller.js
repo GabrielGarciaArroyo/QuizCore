1460d2f7bd3cc545dd4680279fd2c971e629b668
GabrielGarciaArroyo <gabriel.garcia.arroyo@alumnos.upm.es>
2015-05-19 10:08:28 +0200

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,9 @@
+var models = require('../models/models.js');
+
 exports.question = function(req, res) {
-    res.render('quizes/question' , {pregunta: 'Capital de Italia'});
+models.Quiz.findAll().then(function(quiz){
+ res.render('quizes/question' , {pregunta: quiz[0].pregunta})
+})
 };
 
 exports.author = function(req, res) {
@@ -7,9 +11,11 @@ res.render('author', { alumnos: 'Gabriel Garcia Arroyo y Eduardo Sánchez Clemen
 
 };
 exports.answer = function(req , res) {
-   if (req.query.respuesta === 'Roma') {
+models.Quiz.findAll().then(function(quiz){
+   if (req.query.respuesta === quiz[0].respuesta) {
       res.render('quizes/answer' , {respuesta: 'Correcto'});
 } else {
    res.render('quizes/answer' , {respuesta: 'Incorrecto'});
 }
+})
 };
