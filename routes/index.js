ce75ae814cdeaca08b3b0837ac57f8f14499407a
GabrielGarciaArroyo <gabriel.garcia.arroyo@alumnos.upm.es>
2015-05-25 16:27:07 +0200

    registro y gestión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
+var userController = require('../controllers/user_controller');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: []});
@@ -10,14 +11,21 @@ router.get('/', function(req, res) {
 
 router.param('quizId',quizController.load);
 router.param('commentId', commentController.load);  // autoload :commentId
+router.param('userId', userController.load);  // autoload :userId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
 router.post('/login', sessionController.create);  // crear sesión
 router.get('/logout', sessionController.destroy); // destruir sesión
 
-router.get('/quizes', quizController.index);
+// Definición de rutas de cuenta
+router.get('/user',  userController.new);     // formulario sign un
+router.post('/user',  userController.create);     // registrar usuario
+router.get('/user/:userId(\\d+)/edit',  sessionController.loginRequired, userController.edit);     // editar información de cuenta
+router.put('/user/:userId(\\d+)',  sessionController.loginRequired, userController.update);     // actualizar información de cuenta
+router.delete('/user/:userId(\\d+)',  sessionController.loginRequired, userController.destroy);     // borrar cuenta
 
+router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)' , quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new', 				   sessionController.loginRequired, quizController.new);
