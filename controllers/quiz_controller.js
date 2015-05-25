7deac3e66d62c47d948dadfab3e70439cf8f699d
GabrielGarciaArroyo <gabriel.garcia.arroyo@alumnos.upm.es>
2015-05-25 15:55:49 +0200

    Tabla de usuarios

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -42,23 +42,24 @@ res.render('quizes/index.ejs', {quizes: quizes, errors: []});
 }).catch(function(error){next(error);})
 };
 
+// POST /quizes/create
 exports.create = function(req, res) {
+  req.body.quiz.UserId = req.session.user.id;
   var quiz = models.Quiz.build( req.body.quiz );
-quiz
-.validate()
-.then(
-function(err){
-if(err){
-res.render('quizes/new', {quiz: quiz, errors: err.errors});
-}else{
- quiz
- .save({fields: ["pregunta", "respuesta"]})
- .then(function(){
-    res.redirect('/quizes')  
-  })  
-}
-}
-);
+
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta", "UserId"]})
+        .then( function(){ res.redirect('/quizes')}) 
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  ).catch(function(error){next(error)});
 };
 exports.answer = function(req , res) {
 var resultado = 'Incorrecto';
