080f7efda1aad3e2f038aa9a9f5d58675b3f5536
GabrielGarciaArroyo <gabriel.garcia.arroyo@alumnos.upm.es>
2015-05-25 10:55:26 +0200

    moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.get('/', function(req, res) {
 });
 
 router.param('quizId',quizController.load);
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -19,6 +20,11 @@ router.get('/quizes', quizController.index);
 
 router.get('/quizes/:quizId(\\d+)' , quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', 				   sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',              sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',  quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
@@ -26,6 +32,9 @@ router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+	                                    sessionController.loginRequired, commentController.publish);
+
 router.get('/author', quizController.author );
 
 module.exports = router;
